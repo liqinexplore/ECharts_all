a01b4a7743cf8dc439e129a57494ede87917bdce
qingege <liqintiantai@163.com>
2017-05-11 11:37:08 +0800

    提交修改

--- js/jz_all.js
+++ js/jz_all.js
@@ -10,14 +10,16 @@ $first.animate({
 });
 };
 var top_left=[];
-
 setInterval(function(){doscroll()}, 2000);
+var top1_data_all=[{"sum1":20,"sum2":80},{"sum1":30,"sum2":70},{"sum1":60,"sum2":40}]
 var jz_left_top = echarts.init(document.getElementById('jz_left_top'), 'dark');
 var jz_left_top2 = echarts.init(document.getElementById('jz_left_top2'), 'dark');
-var jz_left_top4 = echarts.init(document.getElementById('jz_left_top4'), 'dark');
-var jz_left_top9 = echarts.init(document.getElementById('jz_left_top9'), 'dark');
+var jz_top4_left1 = echarts.init(document.getElementById('jz_top4_left1'), 'dark');
+var jz_top4_left2 = echarts.init(document.getElementById('jz_top4_left2'), 'dark');
+var jz_top4_left3 = echarts.init(document.getElementById('jz_top4_left3'), 'dark');
+var top3_center_tu = echarts.init(document.getElementById('top3_center_tu'), 'dark');
 var jz_left_top5 = echarts.init(document.getElementById('jz_left_top5'), 'dark');
-// var jz_left_top6 = echarts.init(document.getElementById('jz_left_top6'), 'dark');
+var jz_top6_zhe = echarts.init(document.getElementById('jz_top6_zhe'), 'dark');
 // var jz_content_top = echarts.init(document.getElementById('jz_content_top'), 'dark');
 // var jz_right_top = echarts.init(document.getElementById('jz_right_top'), 'dark');
 // var jz_right_center = echarts.init(document.getElementById('jz_right_center'), 'dark');
@@ -66,17 +68,17 @@ option_jz_left_top1 = {
             name:'Line 1',
             type:'pie',
             clockWise:false,
-            radius : [50,38],
+            radius : ['48%', '56%'],
             itemStyle : dataStyle,
             hoverAnimation: false,
 
             data:[
                 {
-                    value:300,
+                    value:top1_data_all[0].sum1,
                     name:'01',
                 },
                 {
-                    value:50,
+                    value:top1_data_all[0].sum2,
                     name:'invisible',
                     itemStyle : placeHolderStyle
                 }
@@ -87,17 +89,17 @@ option_jz_left_top1 = {
             name:'Line 2',
             type:'pie',
             clockWise:false,
-            radius : [65,54],
+            radius : ['60%', '68%'],
             itemStyle : dataStyle,
             hoverAnimation: false,
 
             data:[
                 {
-                    value:150,
+                    value:top1_data_all[1].sum1,
                     name:'02'
                 },
                 {
-                    value:60,
+                    value:top1_data_all[1].sum2,
                     name:'invisible',
                     itemStyle : placeHolderStyle
                 }
@@ -108,16 +110,16 @@ option_jz_left_top1 = {
             type:'pie',
             clockWise:false,
             hoverAnimation: false,
-            radius : [80, 70],
+            radius : ['72%', '80%'],
             itemStyle : dataStyle,
 
             data:[
                 {
-                    value:80,
+                    value:top1_data_all[2].sum1,
                     name:'03'
                 },
                 {
-                    value:50,
+                    value:top1_data_all[2].sum2,
                     name:'invisible',
                     itemStyle : placeHolderStyle
                 }
@@ -317,7 +319,7 @@ option_jz_left_top2 = {
     }]
 };
 
-option_jz_left_top9 = {
+option_top3_center_tu = {
     color: ['#00ffff', '#00ffa2', '#f0e750'],
     tooltip: {
         trigger: 'axis',
@@ -462,7 +464,8 @@ option_jz_left_top9 = {
 
         ] //series结束
 }; // option结束
-option_jz_left_top4 = {
+// -------------------------考勤统计小圆圈11-------------------------------------
+option_jz_top4_left1 = {
     tooltip: {
         trigger: 'item',
         formatter: "{a} <br/>{b} : {c} ({d}%)"
@@ -470,7 +473,7 @@ option_jz_left_top4 = {
     legend: {
         orient: 'vertical',
         left: 'left',
-        data: ['占有率']
+        data: ['']
     },
     series: [{
         name: '饼图二',
@@ -533,13 +536,370 @@ option_jz_left_top4 = {
         }]
     }]
 };
+// -----------------------考勤统计小圆圈结束--------------------------
+
+// -----------------------考勤统计小圆圈22222--------------------------
+
+option_jz_top4_left2 = {
+    tooltip: {
+        trigger: 'item',
+        formatter: "{a} <br/>{b} : {c} ({d}%)"
+    },
+    legend: {
+        orient: 'vertical',
+        left: 'left',
+        data: ['']
+    },
+    series: [{
+        name: '饼图二',
+        type: 'pie',
+        radius: ['60%', '70%'],
+        label: {
+            normal: {
+                position: 'center'
+            }
+        },
+        data: [{
+            value: 40,
+            name: '占有率',
+            label: {
+                normal: {
+                    formatter: '{d} %',
+                    textStyle: {
+                        fontSize: 20,
+                        color:'red'
+                    }
+                }
+            },
+             itemStyle: {
+                 normal: {
+                    color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+                        offset: 0,
+                        color: 'rgba(207, 8,255, 1)'
+                    }, {
+                        offset: 1,
+                        color: 'rgba(17, 99,198, 0.4)'
+                    }]),
+                    shadowColor: 'rgba(0, 0, 0, 1)',
+                    shadowBlur: 10
+                },
+            }
+        }, {
+            value: 60,
+            name: '占位',
+            label: {
+                normal: {
+                    formatter: '\n完成率',
+                    textStyle: {
+                        color: '#555',
+                        fontSize: 16
+                    }
+                }
+            },
+            tooltip: {
+                show: false
+            },
+            itemStyle: {
+                normal: {
+                    color: '#aaa'
+                },
+                emphasis: {
+                    color: '#aaa'
+                }
+            },
+            hoverAnimation: false
+        }]
+    }]
+};
+// -----------------------考勤统计小圆圈结束--------------------------
+
+// -----------------------考勤统计小圆圈333333--------------------------
+
+option_jz_top4_left3 = {
+    tooltip: {
+        trigger: 'item',
+        formatter: "{a} <br/>{b} : {c} ({d}%)"
+    },
+    legend: {
+        orient: 'vertical',
+        left: 'left',
+        data: ['']
+    },
+    series: [{
+        name: '饼图二',
+        type: 'pie',
+        radius: ['60%', '70%'],
+        label: {
+            normal: {
+                position: 'center'
+            }
+        },
+        data: [{
+            value: 40,
+            name: '占有率',
+            label: {
+                normal: {
+                    formatter: '{d} %',
+                    textStyle: {
+                        fontSize: 20,
+                        color:'red'
+                    }
+                }
+            },
+             itemStyle: {
+                 normal: {
+                    color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+                        offset: 0,
+                        color: 'rgba(207, 8,255, 1)'
+                    }, {
+                        offset: 1,
+                        color: 'rgba(17, 99,198, 0.4)'
+                    }]),
+                    shadowColor: 'rgba(0, 0, 0, 1)',
+                    shadowBlur: 10
+                },
+            }
+        }, {
+            value: 60,
+            name: '占位',
+            label: {
+                normal: {
+                    formatter: '\n完成率',
+                    textStyle: {
+                        color: '#555',
+                        fontSize: 16
+                    }
+                }
+            },
+            tooltip: {
+                show: false
+            },
+            itemStyle: {
+                normal: {
+                    color: '#aaa'
+                },
+                emphasis: {
+                    color: '#aaa'
+                }
+            },
+            hoverAnimation: false
+        }]
+    }]
+};
+// -----------------------考勤统计小圆圈结束--------------------------
+// -----------------------折线图--------------------------
+option_jz_top6_zhe = {
+    color: ['#00ffff', '#00ffa2', '#f0e750'],
+    tooltip: {
+        trigger: 'axis',
+        textStyle: {
+            fontSize: 15,
+            color: "#fff",
+        }
+    },
+    legend: {
+        orient: 'horizontal',
+        right: "4%",
+        itemGap: 20,
+        //itemWidth:16,
+        //itemHeight:12,
+        data: ['高危', '中危', '低危'],
+        textStyle: {
+            color: '#fff',
+        }
+    },
+    toolbox: {
+        show: true,
+        orient: 'vertical',
+        right: 'right',
+        top: '20%',
+        itemGap: 20,
+        feature: {
+            magicType: {
+                show: true,
+                type: ['line', 'bar']
+            },
+            restore: {
+                show: true
+            },
+            saveAsImage: {
+                show: true
+            }
+        },
+        iconStyle: {
+            normal: {
+                color: '#0390e9'
+            }
+        }
+    },
+    grid: {
+        show: true,
+        left: 60,
+        top: 34,
+        right: 44,
+        bottom: 42,
+        borderWidth: 1,
+        borderColor: 'rgba(170,172,178,0.33)',
+        backgroundColor: 'rgba(17, 34, 69, 0.24)',
+    },
+    calculable: true,
+    xAxis: [{
+        type: 'category',
+        boundaryGap: false,
+        //在（type: 'category'）中设置data有效
+        data: ['00:00:00', '03:00:00', '06:00:00', '09:00:00', '12:00:00',
+            '15:00:00', '18:00:00', '21:00:00', '00:00:00'
+        ],
+
+        splitLine: { //坐标轴在 grid 区域中的分隔线；
+            show: true,
+            lineStyle: { //分割线颜色，可设单个，也可以设置数组。
+                color: 'rgba(170,172,178,0.33)'
+            }
+        },
+        axisLine: { //坐标轴轴线相关设置。就是数学上的x轴
+            show: true,
+            lineStyle: {
+                color: 'rgba(170,172,178,0.53)'
+            },
+        },
+        axisLabel: {
+            textStyle: {
+                color: 'rgba(255,255,255,0.83)',
+                fontSize: 15,
+            },
+        },
+    }],
+    yAxis: [{
+        type: 'value',
+        min: 0,
+        max: 4000,
+        splitLine: {
+            show: true,
+            lineStyle: {
+                color: 'rgba(170,172,178,0.33)'
+            }
+        },
+        axisLine: { //坐标轴轴线相关设置。就是数学上的y轴
+            show: true,
+            lineStyle: {
+                color: 'rgba(170,172,178,0.53)'
+            },
+        },
+        axisLabel: {
+            textStyle: {
+                color: 'rgba(255,255,255,0.83)',
+                fontSize: 12,
+            },
+        },
+        splitArea: {
+            show: true,
+            areaStyle: {
+                color: ['#112245', 'rgba(34,50,82,0.4)']
+            }
+        }
+    }],
+    series: [{
+                name: '高危',
+                type: 'line',
+                smooth: true, //是否平滑曲线显示
+                lineStyle: { //线条样式
+                    normal: {
+                        width: 1
+                    }
+                },
+                areaStyle: { //区域填充样式
+                    normal: {
+                        //线性渐变，前4个参数分别是x0,y0,x2,y2(范围0~1);相当于图形包围盒中的百分比。如果最后一个参数是‘true’，则该四个值是绝对像素位置。
+                        color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+                            offset: 0,
+                            color: 'rgba(0, 255, 255, 0.9)'
+                        }, {
+                            offset: 0.7,
+                            color: 'rgba(0, 255, 255, 0.6)'
+                        }], false),
+
+                        shadowColor: 'rgba(0, 0, 0, 0.9)', //阴影颜色
+                        shadowBlur: 10 //shadowBlur设图形阴影的模糊大小。配合shadowColor,shadowOffsetX/Y, 设置图形的阴影效果。
+                    }
+                },
+                itemStyle: { //折现拐点标志的样式
+                    normal: {
+                        color: 'rgb(0,255,255)'
+                    }
+                },
+                data: [2000, 122, 3121, 54, 60, 2630, 1150, 2442, 1292]
+            }, {
+                name: '中危',
+                type: 'line',
+                smooth: true,
+                lineStyle: {
+                    normal: {
+                        width: 1
+                    }
+                },
+                areaStyle: {
+                    normal: {
+                        color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+                            offset: 0,
+                            color: 'rgba(240, 231, 80, 0.9)'
+                        }, {
+                            offset: 0.8,
+                            color: 'rgba(240, 231, 80, 0.4)'
+                        }], false),
+                        shadowColor: 'rgba(0, 0, 0, 0.9)',
+                        shadowBlur: 10
+                    }
+                },
+                itemStyle: {
+                    normal: {
+                        color: 'rgb(240,231,80)'
+
+                    }
+                },
+                data: [1130, 812, 1134, 2361, 413, 1330, 1301, 594, 1230]
+            }, {
+                name: '低危',
+                type: 'line',
+                smooth: true,
+                lineStyle: {
+                    normal: {
+                        width: 1
+                    }
+                },
+                itemStyle: {
+                    normal: {
+                        color: 'rgb(0,255,162)'
+                    }
+                },
+                areaStyle: {
+                    normal: {
+                        color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+                            offset: 0,
+                            color: 'rgba(0,255,162, 0.8)'
+                        }, {
+                            offset: 0.8,
+                            color: 'rgba(0,255,162, 0.4)'
+                        }], false),
+                        shadowColor: 'rgba(0, 0, 0, 0.9)',
+                        shadowBlur: 10
+                    }
+                },
+                data: [1220, 1132, 1421, 2114, 954, 620, 132, 190, 540]
+            },
+
+        ] //series结束
+}; // option结束
+//------------------------------------------------
 // 使用刚指定的配置项和数据显示图表。
 jz_left_top.setOption(option_jz_left_top1);
 jz_left_top2.setOption(option_jz_left_top2);
-jz_left_top4.setOption(option_jz_left_top4);
-jz_left_top9.setOption(option_jz_left_top9);
+jz_top4_left1.setOption(option_jz_top4_left1);
+jz_top4_left2.setOption(option_jz_top4_left2);
+jz_top4_left3.setOption(option_jz_top4_left3);
+top3_center_tu.setOption(option_top3_center_tu);
 jz_left_top5.setOption(option_jz_left_top6);
-// jz_left_top6.setOption(option_jz_left_top6);
+jz_top6_zhe.setOption(option_jz_top6_zhe);
 // jz_content_top.setOption(option_jz_content_top);
 // jz_right_top.setOption(option_jz_right_top);
 // jz_right_center.setOption(option_jz_right_center);
